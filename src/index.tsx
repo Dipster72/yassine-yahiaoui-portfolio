980629cfb4c7efac55e59c3daf8bd647eca8ede7
Yassine Yahiaoui <yahiaoui.yassine98@gmail.com>
2025-10-30 14:13:59 +0100

    Update and rename index.js to index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom'; // Notez que nous importons directement de 'react-dom'
+
+// C'est la ligne à noter : elle remonte d'un niveau (..) pour trouver App.tsx à la racine
+import App from '../App'; 
+
+// Cible l'élément HTML avec l'ID 'root'
+const container = document.getElementById('root');
+
+// Vérifie que l'élément existe avant de continuer
+if (container) {
+  // Rendu en utilisant la méthode de rendu plus ancienne et plus compatible
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    container
+  );
+} else {
+  console.error('Root element not found in index.html');
+}
