f9e8910ff2f9038dcec1e07d1e71f59b97f6c786
Yassine Yahiaoui <yahiaoui.yassine98@gmail.com>
2025-10-30 17:05:52 +0100

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,6 +1,6 @@
 import { Card, CardContent, CardHeader } from './ui/card';
 import { Badge } from './ui/badge';
-import { Button } from './ui/button';
+import { Button } from './ui/Button';
 import { ImageWithFallback } from './figma/ImageWithFallback';
 
 // Simple SVG icons to replace lucide-react
